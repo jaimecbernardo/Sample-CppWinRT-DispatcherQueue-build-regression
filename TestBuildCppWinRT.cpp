c1a7221f9eeb6dab8a16fa6b812d97eb5132902c
Jaime Bernardo <jaime@janeasystems.com>
2024-02-09 16:47:45 +0000

    Add code that compiled before

--- TestBuildCppWinRT.cpp
+++ TestBuildCppWinRT.cpp
@@ -1,6 +1,7 @@
 // TestBuildCppWinRT.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include "pch.h"
 #include <iostream>
 
 int main()
---
+++ pch.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+// Windows
+#include <windows.h>
+#include <windowsx.h>
+
+// Must come before C++/WinRT
+#include <wil/cppwinrt.h>
+
+// WinRT
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.Numerics.h>
+#include <winrt/Windows.System.h>
+#include <winrt/Windows.UI.Core.h>
+#include <winrt/Windows.UI.h>
+#include <winrt/Windows.UI.Composition.h>
+#include <winrt/Windows.UI.Composition.Desktop.h>
+#include <winrt/Windows.UI.Popups.h>
+#include <winrt/Windows.Graphics.Capture.h>
+#include <winrt/Windows.Graphics.DirectX.h>
+#include <winrt/Windows.Graphics.DirectX.Direct3d11.h>
+
+// WIL
+#include <wil/resource.h>
+
+#include "dispatcherQueue.desktop.interop.h"
